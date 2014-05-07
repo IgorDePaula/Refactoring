d1e66c0f3bc741fd065c3998553e4c1dc82a104c
Igor De Paula <principe.borodin@gmail.com>
2014-05-07 08:46:11 -0300

    Third test

--- tests/TrianguloTest.php
+++ tests/TrianguloTest.php
@@ -13,5 +13,10 @@ class TrianguloTest extends PHPUnit_Framework_Testcase{
         $t = new Triangulo(2,3,4,5,8);
         $this->assertTrue(true,$t->isEscaleno());
     }
+    public function testIsEquilatero()
+    {
+        $t = new Triangulo(2,2,2,5,8);
+        $this->assertTrue(true,$t->isEquilatero());
+    }
     
 }
