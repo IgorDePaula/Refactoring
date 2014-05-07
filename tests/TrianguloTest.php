d5490262cee9e2594f2bd897e2896a217e9a321c
Igor De Paula <principe.borodin@gmail.com>
2014-05-07 09:16:04 -0300

    Add coverage code

--- tests/TrianguloTest.php
+++ tests/TrianguloTest.php
@@ -3,16 +3,27 @@
 include '../Triangulo.php';
 class TrianguloTest extends PHPUnit_Framework_Testcase{
     
+    /**
+     * @covers Triangulo::isTriangulo
+     */
     public function testIsTriangulo()
     {
         $t = new Triangulo(2,3,4,5,8);
         $this->assertTrue(true,$t->isTriangulo());
     }
+    
+    /**
+     * @covers Triangulo::isEscaleno
+     */
     public function testIsEscaleno()
     {
         $t = new Triangulo(2,3,4,5,8);
         $this->assertTrue(true,$t->isEscaleno());
     }
+    
+    /**
+     * @covers Triangulo::isEquilatero
+     */
     public function testIsEquilatero()
     {
         $t = new Triangulo(2,2,2,5,8);
--- tests/criaregistro.sh
+++ tests/criaregistro.sh
@@ -6,6 +6,6 @@ dir="build-$data"
 mkdir $dir
 phpunit --debug --coverage-html="$dir/coverage"  --coverage-text="$dir/result.txt" --coverage-xml $dir --coverage-clover "$dir/coverage.xml"   ./ > $dir/phpunit.txt
 pdepend --jdepend-chart="$dir/chart-depend.svg" --overview-pyramid="$dir/overview.svg" --coverage-report="$dir/coverage.xml" ./
-phploc --names-exclude="*Test.php" ./ > $dir/phploc.txt
+phploc ../Triangulo.php --names-exclude="*Test.php" ./ > $dir/phploc.txt
 phpmd ./*.php html codesize,unusedcode,cleancode,design > $dir/phpmd.html
 phpcpd ./ --names="*.php" --names-exclude="*Test.php" --log-pmd="$dir/phpcpd.xml" > $dir/phpcpd.txt
